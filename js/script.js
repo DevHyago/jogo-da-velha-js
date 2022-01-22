8edd0ff19c5e351ab191a775d9807dbd930c1c8f
Hyago Vieira <dev.hyago@outlook.com>
2022-01-22 08:47:01 -0300

    Criando logica do vencedor

--- js/script.js
+++ js/script.js
@@ -29,11 +29,16 @@ for(let i = 0; i < boxes.length; i++){
          }else{
             player2++;
          }
+
+         checkWinCondition();
+
       }
 
    });
 }
 
+
+//Vê quem joga
 function checkEl(player1, player2){
    if(player1 == player2){
       //X
@@ -43,4 +48,153 @@ function checkEl(player1, player2){
       el = o;
    }
    return el;
+}
+
+//Vê quem ganha 
+function checkWinCondition(){
+
+   let b1 = document.getElementById("block-1");
+   let b2 = document.getElementById("block-2");
+   let b3 = document.getElementById("block-3");
+   let b4 = document.getElementById("block-4");
+   let b5 = document.getElementById("block-5");
+   let b6 = document.getElementById("block-6");
+   let b7 = document.getElementById("block-7");
+   let b8 = document.getElementById("block-8");
+   let b9 = document.getElementById("block-9");
+
+   //Verificando 1° linha na horizontal
+   if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0){
+      let b1Child = b1.childNodes[0].className;
+      let b2Child = b2.childNodes[0].className;
+      let b3Child = b3.childNodes[0].className;
+
+      if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 2° linha na horizontal
+   if(b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0){
+      let b4Child = b4.childNodes[0].className;
+      let b5Child = b5.childNodes[0].className;
+      let b6Child = b6.childNodes[0].className;
+
+      if(b4Child == 'x' && b5Child == 'x' && b6Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 3° linha na horizontal
+   if(b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length > 0){
+      let b7Child = b7.childNodes[0].className;
+      let b8Child = b8.childNodes[0].className;
+      let b9Child = b9.childNodes[0].className;
+
+      if(b7Child == 'x' && b8Child == 'x' && b9Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 1° linha na vertical
+   if(b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0){
+      let b1Child = b1.childNodes[0].className;
+      let b4Child = b4.childNodes[0].className;
+      let b7Child = b7.childNodes[0].className;
+
+      if(b1Child == 'x' && b4Child == 'x' && b7Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b1Child == 'o' && b4Child == 'o' && b7Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 2° linha na vertical
+   if(b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0){
+      let b2Child = b2.childNodes[0].className;
+      let b5Child = b5.childNodes[0].className;
+      let b8Child = b8.childNodes[0].className;
+
+      if(b2Child == 'x' && b5Child == 'x' && b8Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b2Child == 'o' && b5Child == 'o' && b8Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 3° linha na vertical
+   if(b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0){
+      let b3Child = b3.childNodes[0].className;
+      let b6Child = b6.childNodes[0].className;
+      let b9Child = b9.childNodes[0].className;
+
+      if(b3Child == 'x' && b6Child == 'x' && b9Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b3Child == 'o' && b6Child == 'o' && b9Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 1° diagonal
+   if(b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0){
+      let b1Child = b1.childNodes[0].className;
+      let b5Child = b5.childNodes[0].className;
+      let b9Child = b9.childNodes[0].className;
+
+      if(b1Child == 'x' && b5Child == 'x' && b9Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b1Child == 'o' && b5Child == 'o' && b9Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Verificando 2° diagonal
+   if(b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0){
+      let b3Child = b3.childNodes[0].className;
+      let b5Child = b5.childNodes[0].className;
+      let b7Child = b7.childNodes[0].className;
+
+      if(b3Child == 'x' && b5Child == 'x' && b7Child == 'x'){
+         //X vence
+         console.log(`X venceu!!`);
+      }else if(b3Child == 'o' && b5Child == 'o' && b7Child == 'o'){
+         //O vence
+         console.log(`O venceu!!`);
+      }
+   }
+
+   //Deu velha
+   let counter = 0;
+   for(let i = 0; i < boxes.length; i++){
+      if(boxes[i].childNodes[0] != undefined){
+         counter++;
+      }
+   }
+
+   if(counter == 9){
+      console.log("Deu velha");
+   }
+
+
+
 }
